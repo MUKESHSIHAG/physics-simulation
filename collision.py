c9eac690cbbb7b7ba3781b00ba82c9a3f480c29c
MUKESHSIHAG <sihagmukesh22@gmail.com>
2018-10-24 22:44:20 +0530

    added movements

--- collision.py
+++ collision.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 background_color = (255,255,255)
 (width, height) = (400, 300)
@@ -11,13 +12,36 @@ class Particle:
         self.size = size
         self.color = (10,20,55)
         self.thickness = 1
-    def display(self):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.size, self.thickness)
+        self.speed = 0.1
+        self.angle = 0.1
         
+    def display(self):
+        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.size, self.thickness)
+
+    def move(self):
+        self.x += math.sin(self.angle)*self.speed
+        self.y += math.cos(self.angle)*self.speed
+
+    def bounce(self):
+        if self.x > width - self.size:
+            self.x = 2 * (width - self.size) - self.x
+            self.angle = -self.angle
+
+        elif self.x < self.size:
+            self.x = 2 * self.size - self.x
+            self.angle = -self.angle
+
+        if self.y > height - self.size:
+            self.y = 2 * (height - self.size) - self.y
+            self.angle = math.pi - self.angle
+
+        elif self.y < self.size:
+            self.y = 2 * self.size - self.y
+            self.angle = math.pi - self.angle
+
 
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption('collision')
-screen.fill(background_color)
 
 number_of_particles = 10
 my_particles = []
@@ -26,15 +50,23 @@ for n in range(number_of_particles):
     size = random.randint(10,20)
     x = random.randint(size, width-size)
     y = random.randint(size, height-size)
-    my_particles.append(Particle((x,y),size))
 
-for particle in my_particles:
-    particle.display()
+    particle = Particle((x,y),size)
+    particle.speed = random.random()
+    particle.angle = random.uniform(0, math.pi*2)
     
-pygame.display.flip()
+    my_particles.append(particle)
 
 running  = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.fill(background_color)
+
+    for particle in my_particles:
+        particle.move()
+        particle.bounce()
+        particle.display()
+    pygame.display.flip()
+
