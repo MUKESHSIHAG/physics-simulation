8114143b59c9198c74ecc54c4732b817041dac28
Tharun Ravuri <41537996+tharun143@users.noreply.github.com>
2018-12-13 12:35:13 +0530

    Update gravity.py

--- Gravitation/gravity.py
+++ Gravitation/gravity.py
@@ -1,7 +1,4 @@
-import random
-import pygame
-import PyParticles
-
+import random,pygame,PyParticles
 (width, height) = (400, 400)
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Star formation')
